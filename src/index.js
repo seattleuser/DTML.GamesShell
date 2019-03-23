9f4a2df93d2f90b21c2fea42da64c784dbd20e0b
Aleksey <aleksin@dtml.org>
2019-03-23 16:20:37 -0700

    share

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from 'react-snapshot';
-import App from './App';
+import ReactDOM from 'react-dom';
 import './index.css';
-render(
-  <App />,
-  document.getElementById('root')
-);
+import App from './App';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
